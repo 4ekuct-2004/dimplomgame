92d26cf20611bb5aca451fc26e7116c0f496a6d3
Alexey <aleshkaxxl123@gmail.com>
2025-03-12 00:26:39 +0700

    главное меню, худ, противник ближнего боя

--- debug/full/main.gd
+++ debug/full/main.gd
@@ -7,11 +7,29 @@ var map_builder = MapBuilder.new()
 @onready var player = $Player
 var enemies = load("res://objects/creatures/npc/enemies/ghost/enemy_ghost.tscn")
 
+var paused = false
+signal pause
+
 func _ready() -> void:
 	var map = map_generator.generate_map()
 	map_builder.generate_from_map(map, self)
 	
 	player.position = map_builder.start_position
-	var enemy = enemies.instantiate()
-	add_child(enemy)
-	enemy.position = player.position + Vector2(500, 0)
+	print(player.position)
+	pause.connect(on_pause)
+
+
+func on_pause():
+	paused = not paused
+	
+	var children = get_children()
+	if paused:
+		for child in children:
+			if child is not Node or child is Control: continue
+			child.set_process(false)
+			child.set_physics_process(false)
+	else:
+		for child in children:
+			if child is not Node or child is Control: continue
+			child.set_process(true)
+			child.set_physics_process(true)
